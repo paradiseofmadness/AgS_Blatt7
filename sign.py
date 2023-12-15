3dbcff13b871b879b43341abba161bd868c8e471
Paradise of Madness <141437346+paradiseofmadness@users.noreply.github.com>
2023-12-15 08:45:11 +0100

    Update sign.py

--- sign.py
+++ sign.py
@@ -1,9 +1,10 @@
 def sign(sigma):
   res = 0
-  for i in sigma[1]:
-    for j in sigma[1]:
-      if i>j:
-        res += (sigma[2][sigma[1].indexof(i)]-sigma[2][sigma[1].indexof(j)])/(i-j)
+  for i in sigma[0]:
+    for j in sigma[0]:
+      if i<j:
+        res += (sigma[1][sigma[0].index(i)]-sigma[1][sigma[0].index(j)])/(i-j)
+  return res
 
 # aus 4.a)
 kappa = [[1,2,3,4],[3,4,2,1]]
